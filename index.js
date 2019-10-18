7dc34453ee7dda50f5df886add9b96f80580606e
Daniel Nix <dnix7774@gmail.com>
2019-10-17 19:43:47 -0500

    Fixed addemoji.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const emojiBotMethods = require('./node_modules/emojibot-methods');
 
-const token = 'NjM0MTcwODkxMTM1MzUyODM0.XaentQ.08T5Faiis48Ib3ITO2s_yu-bo5E';
+const token = 'NjM0MTcwODkxMTM1MzUyODM0.Xaj2XQ.HI5Pp9Q5CfCJr_UsonDuasVMieY';
 
 client.on('ready', () => {
     console.log("this bot is online");
